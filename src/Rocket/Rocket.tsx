0bac7c930e0a831bf7159012304a8fb6ba9bec20
Jackson Davis <jwd0526@gmail.com>
2024-03-11 20:28:43 -0400

    minor bug fixes

--- src/Rocket/Rocket.tsx
+++ src/Rocket/Rocket.tsx
@@ -9,14 +9,14 @@ interface RocketProps {
     levelDispatch: React.Dispatch<Action>;
 }
 
-var fuel = 1000;
 const Rocket: React.FC<RocketProps> = ({ levelState, levelDispatch }) => {
   const [launched, setLaunched] = useState<boolean>(false);
-  const [speed, setSpeed] = useState<number>(3);
+  const [speed, setSpeed] = useState<number>(5);
   const [incrementProgress, setIncrementProgress] = useState<number>(0);
   const [color, setColor] = useState<number>(80);
   const [height, setHeight] = useState<number>(0);
-  const acceleration = .05;
+  const [acceleration, setAcceleration] = useState<number>((levelState.power / 6) * .1);
+  const [fuel, setFuel] = useState<number>(0);
   const maxSpeed: number = 100;
   const backGroundIncrement: number = 2000;
   
@@ -40,7 +40,7 @@ const Rocket: React.FC<RocketProps> = ({ levelState, levelDispatch }) => {
       } else {
         setIncrementProgress(incrementProgress + speed);
       }
-      if (fuel === 0) {
+      if (parseInt(fuel.toFixed(0)) === Math.round(levelState.capacity * 500)) {
         setSpeed(0);
         setLaunched(false);
         rocket.classList.remove("launched");
@@ -48,7 +48,7 @@ const Rocket: React.FC<RocketProps> = ({ levelState, levelDispatch }) => {
         rocket.classList.remove("rocket-active");
         rocket.classList.add("falling-and-spinning");
       } else {
-        fuel = fuel - 1;
+        setFuel(fuel + 1);
       }
       const blank = document.createElement('div');
 
@@ -63,23 +63,34 @@ const Rocket: React.FC<RocketProps> = ({ levelState, levelDispatch }) => {
   }
 
   useEffect(() => {
+    setAcceleration((levelState.power / 6) * .15);
     if (launched) {
       const interval = setInterval(() => {
         launch();
-      }, 16);
+      }, 40);
       return () => clearInterval(interval);
     }
-  }, [launched, speed, setSpeed, fuel]);
+  }, [launched, speed, setSpeed, levelState.power]);
 
  
   return (
     <>
       <div className="launch-button">
-        <img className="clickable-launch" src={play1} alt="launch-button" onClick={() => {setLaunched(true)}} />
+        <img className="clickable-launch" src={play1} alt="launch-button" onClick={() => {
+            setLaunched(true);
+            const pan = document.getElementById("panel-img");
+            const colImg = document.getElementById("collapse-img");
+            const col = document.getElementById("collapse");
+            pan?.classList.add("collapse");
+            colImg?.classList.add("rotate");
+            col?.classList.add("collapse");
+            }} />
       </div>
         <div className="rocket-box" id='rocket'>
             <img className="rocket-img" src={rocketInactive} alt='rocket-img'/>
+            
         </div>
+        <p style={{ position:"fixed", bottom: "5%", left: "5%" }}>Acceleration: {(parseFloat(acceleration.toFixed(3)) + 1).toFixed(3)}<br/>Height: {parseInt(height.toFixed(3))/10}<br/>Fuel: {levelState.capacity * 500 - fuel}</p>
     </>
   );
 };
